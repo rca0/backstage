0b9640dd121a5de20de67c7b9b8c7d4c7a240fbc
Ruan Arcega <ruanarcega@gmail.com>
2023-12-21 19:32:46 -0300

    Remove tf codes

--- if.tf
+++
@@ -1,4 +0,0 @@
-{%- if values.vpc %}
-  vpc_subnet_ids                          = ["subnet-000000aaaaaa00000"]
-  attach_network_policy                   = true
-  vpc_security_group_ids                  = [aws_security_group.security_group.id]{% endif %}
